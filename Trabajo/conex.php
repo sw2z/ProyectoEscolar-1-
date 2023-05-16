29a0cb4b877e8d3407068d7b37e672e68556d982
sw2z <118495710+sw2z@users.noreply.github.com>
2023-05-16 18:03:59 -0400

    Add files via upload

---
+++ Trabajo/conex.php
@@ -0,0 +1,16 @@
+<?php
+//parametros de conex
+$host = "localhost";
+$user = "root";
+$pass = "";
+$db = "test_2";
+
+$conn = mysqli_connect($host,$user,$pass,$db);
+
+/*if($conn){
+    echo  "se conecto exitosamente";
+}else{
+    echo "no se conecto";
+}
+*/
+?>
